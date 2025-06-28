177e62a81af64441694284f5a230bf87ba27f3ca
SALEH-abdulwahab <saleh200228@gmail.com>
2025-06-28 21:38:03 +0300

    committed

---
+++ src/00-objectball.js
@@ -0,0 +1,41 @@
+gameObject()
+const home = {
+teamName: "Brooklen Nets",
+colors: ["Black, White"],
+players: {
+"alan anderson": {
+Number: "0",
+Shoe: "16",
+points: "22",
+rebonds: "12",
+assits: "12",
+steals: 3,
+blocks: 1,
+slamDunks: 1,
+},
+"reggie evans": {
+Number: 30,
+Shoe: 14,
+points: 12,
+rebonds: 12,
+assits: 12,
+steals: 12,
+blocks: 12,
+slamDunks: 7,
+},
+"brook lopez": {
+Number: 30,
+Shoe: 14,
+points: 12,
+rebonds: 12,
+assits: 12,
+steals: 12,
+blocks: 12,
+slamDunks: 7,
+},
+
+
+
+}
+}
+
